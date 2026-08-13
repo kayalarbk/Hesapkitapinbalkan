699e700ee73d959515d480a8b0588c7ac8ca5972
Barış Kaya <kayalarbk2004@gmail.com>
2026-08-13 21:24:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,10 +6,10 @@ const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-maskable-512.png",
-  "./icons/apple-touch-icon.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-maskable-512.png",
+  "./apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
